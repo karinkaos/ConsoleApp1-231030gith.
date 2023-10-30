1fc7694e05ab2aed6eae22fef20bdd3b9b49cc03
Karin Oskarsson <k.oskarsson@outlook.com>
2023-10-30 10:42:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1_231030gith_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
